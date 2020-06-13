c2c4e45e5ad3145dc078181b61b5b07132b96c6f
IcE <ivica-matic@outlook.com>
2020-06-13 20:55:13 +0100

    Modified models to display name in django dashboard

--- iot_http_server/mqtt_auth/models.py
+++ iot_http_server/mqtt_auth/models.py
@@ -9,6 +9,9 @@ class MQTTUser(models.Model):
     mqtt_password: models.UUIDField = models.UUIDField(default=uuid.uuid4)
     is_mqtt_superuser: models.BooleanField = models.BooleanField(default=False)
 
+    def __str__(self):
+        return str(self.mqtt_username)
+
     class Meta:
         db_table = 'MQTTUsers'
         verbose_name_plural = 'MQTTUsers'
@@ -18,6 +21,9 @@ class Device(models.Model):
     user: models.ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE)
     name: models.CharField = models.CharField(max_length=50)
     mqtt_client_id: models.UUIDField = models.UUIDField(default=uuid.uuid4)
+
+    def __str__(self):
+        return str(self.mqtt_client_id)
 
     class Meta:
         db_table = 'Devices'
@@ -29,6 +35,9 @@ class Data(models.Model):
     timestamp: models.DateTimeField = models.DateTimeField(auto_now_add=True)
     type: models.CharField = models.CharField(max_length=50)
     value: models.TextField = models.TextField()
+
+    def __str__(self):
+        return self.type + " => " + self.value + " @ " + str(self.device.mqtt_client_id)
 
     class Meta:
         db_table = 'Data'
