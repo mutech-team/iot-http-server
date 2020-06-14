5a3b40b323e53fbbc3a5c30daae15de201c28173
Petar Peychev <petarpeychev98@gmail.com>
2020-06-14 15:36:08 +0100

    Implemented register view and logic

--- iot_http_server/dashboard/selectors.py
+++ iot_http_server/dashboard/selectors.py
@@ -4,15 +4,15 @@ from django.http import QueryDict
 from django.contrib.auth import authenticate, login
 from dashboard.forms import LoginForm
 from django.http import HttpRequest
+from django.contrib.auth.models import User
 
 
 def authenticate_user(request: HttpRequest) -> bool:
-    form = LoginForm(request.POST)
+    form: LoginForm = LoginForm(request.POST)
     if form.is_valid():
         user = authenticate(username=form.cleaned_data['username'],
                             password=form.cleaned_data['password'])
         if user is not None:
-            if user.is_active:
-                login(request, user)
-                return True
+            login(request, user)
+            return True
     return False
