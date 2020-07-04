edb3acf3c6ab5289e23e4f88e82833ba4a72ecf1
Petar Peychev <petarpeychev98@gmail.com>
2020-07-04 16:16:44 +0100

    Fixed typing and redesigned some front-end

--- iot_http_server/data/services.py
+++ iot_http_server/data/services.py
@@ -4,11 +4,12 @@ from data import selectors
 import modules.mqtt_publisher as mqtt_publisher
 
 
-def save_state(deviceid: uuid, state: str, value: str) -> None:
-    mqtt_publisher.publish(deviceid, state, value)
+def save_state(deviceid: uuid.UUID, state: str, value: str) -> None:
+    mqtt_publisher.publish(str(deviceid), state, value)
     device = selectors.obtain_device(deviceid)
+    obj: State
+    created: bool
     obj, created = State.objects.update_or_create(device=device, type=state)
-    obj.type = state
     obj.value = value
     obj.serialized = obj.serialize_to_json()
     obj.save()
